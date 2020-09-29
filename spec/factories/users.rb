b93be28d9f53225655ee9d762a7ccdba78757a91
lekegitrepo <lekedevrepo@gmail.com>
2020-09-29 08:31:42 -0500

    write specs for the user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { 'test@gmail.com' }
+    email { 'user@gmail.com' }
     password { '123456789' }
     password_confirmation { '123456789' }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { create :user }
-  # before { user }
-  # subject { user }
+  before { @user = FactoryBot.build(:user) }
+
+  subject { @user }
 
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
+
+  describe 'should validate present of attributes' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should validate_confirmation_of(:password) }
+    it { should allow_value('example@domain.com').for(:email) }
+  end
 end
