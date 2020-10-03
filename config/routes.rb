636088676641a4ef6dd98f197707798d96f347f4
lekegitrepo <lekedevrepo@gmail.com>
2020-10-03 02:55:27 -0500

    set the route to the basic defined path

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ require 'api_constraints'
 
 Rails.application.routes.draw do
   devise_for :users
-  namespace :api, defaults: { format: :json },
-                  constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1,
-          constraints: ApiConstraints.new(version: 1, default: true) do
+  # namespace :api, defaults: { format: :json },
+  #                 constraints: { subdomain: 'api' }, path: '/' do
+  #   scope module: :v1,
+  #         constraints: ApiConstraints.new(version: 1, default: true) do
+  #     resources :users, only: [:show]
+  #   end
+  # end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
       resources :users, only: [:show]
     end
   end
