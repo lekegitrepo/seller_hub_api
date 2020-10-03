a0e4a6f0113e9d9a661325c52bb6aacffe52b8f2
lekegitrepo <lekedevrepo@gmail.com>
2020-10-03 03:42:41 -0500

    add create action method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
   # api/v1/users#show
   def show
     respond_with User.find(params[:id])
