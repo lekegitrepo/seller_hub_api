b4e9fa9e4d1f9c91246d27caaa0502e2b8f8fa09
lekegitrepo <lekedevrepo@gmail.com>
2020-10-03 03:32:29 -0500

    add spec for user's creation

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe Api::V1::UsersController, type: :controller do # RSpec.describe A
 
     it { should respond_with 200 }
   end
+
+  describe 'POST #create user' do
+    context 'When user is successfully created' do
+      before(:each) do
+        @user_attr = FactoryBot.attributes_for :user
+        post :create, { user: @user_attr, format: :json }
+      end
+
+      it 'returns the json representation of the created user record' do
+        user_resp = JSON.parse(response.body, symbolize_names: true)
+        expect(user_resp[:email]).to eql @user_attr[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+  end
 end
