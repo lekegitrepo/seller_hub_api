5e1b8e52de2a0c60e8c85f4f4df4cb989c4d59b1
lekegitrepo <lekedevrepo@gmail.com>
2020-10-05 11:39:33 -0500

    Add destroy session action added

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -37,8 +37,8 @@ RSpec.describe Api::V1::SessionsController do
   describe 'DELETE #destroy' do
     before(:each) do
       @user = FactoryBot.create :user
-      sign_in @user, store: false
-      delete :destroy, id: @user.auth_token
+      sign_in @user
+      delete :destroy, params: { id: @user.auth_token }
     end
 
     it { should respond_with 204 }
