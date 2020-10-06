4bb1e8b066182f94ae64da2b793b30640658bef0
lekegitrepo <lekedevrepo@gmail.com>
2020-10-06 12:44:17 -0500

    update the product serializer

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -12,6 +12,11 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       expect(product_response[:title]).to eql @product.title
     end
 
+    it 'has the user object embedded' do
+      product_response = json_response
+      expect(product_response[:user][:email]).to eql @product.user.email
+    end
+
     it { should respond_with 200 }
   end
 
@@ -24,6 +29,13 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
     it 'returns 4 records from the database' do
       products_response = json_response
       expect(products_response.length).to eql 4
+    end
+
+    it 'returns the user object into each product' do
+      products_response = json_response
+      products_response.each do |product_response|
+        expect(product_response[:user]).to be_present
+      end
     end
 
     it { should respond_with 200 }
