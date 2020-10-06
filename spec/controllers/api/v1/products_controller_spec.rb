707cb564d3a38bea23a2e725b49c3c91364adad3
lekegitrepo <lekedevrepo@gmail.com>
2020-10-06 03:44:00 -0500

    add spec for creating product

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -28,4 +28,43 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
 
     it { should respond_with 200 }
   end
+
+  describe 'POST #create' do
+    context 'when product is successfully created' do
+      before(:each) do
+        user = FactoryBot.create :user
+        @product_attributes = FactoryBot.attributes_for :product
+        api_authorization_header user.auth_token
+        post :create, params: { user_id: user.id, product: @product_attributes }
+      end
+
+      it 'renders the json representation for the created product record' do
+        product_response = json_response
+        expect(product_response[:title]).to eql @product_attributes[:title]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context 'when product is not created' do
+      before(:each) do
+        user = FactoryBot.create :user
+        @invalid_product_attributes = { title: 'Smart TV', price: 'Twelve dollars' }
+        api_authorization_header user.auth_token
+        post :create, params: { user_id: user.id, product: @invalid_product_attributes }
+      end
+
+      it 'renders an errors json' do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on when the user could not be created' do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include 'is not a number'
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end
