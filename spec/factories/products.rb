60a8da652609f8ddfcdcba4b2f93fc68d808c9c9
lekegitrepo <lekedevrepo@gmail.com>
2020-10-06 02:33:25 -0500

    add product model bare bones along with some validations

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :product do
-    title { "MyString" }
-    price { "9.99" }
+    title { FFaker::Product.product_name }
+    price { rand * 100 }
     published { false }
     user_id { 1 }
   end
