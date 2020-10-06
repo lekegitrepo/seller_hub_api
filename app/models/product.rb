4119c5697b6b8d1f8a798369756502febc453539
lekegitrepo <lekedevrepo@gmail.com>
2020-10-06 13:36:56 -0500

    create above_or_equal_to_price scope

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,8 @@ class Product < ApplicationRecord
   scope :filter_by_title, lambda { |keyword|
     where('lower(title) LIKE ?', "%#{keyword.downcase}%")
   }
+
+  scope :above_or_equal_to_price, lambda { |price|
+    where('price >= ?', price)
+  }
 end
