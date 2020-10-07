fb8dd8d882b2e90141d62ddcc9372e6854c9eb4a
lekegitrepo <lekedevrepo@gmail.com>
2020-10-07 03:18:26 -0500

    add order route index

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show, :create, :update, :destroy] do
         resources :products, only: [:create, :update, :destroy]
+        resources :order, only: [:index]
       end
 
       resources :sessions, only: [:create, :destroy]
