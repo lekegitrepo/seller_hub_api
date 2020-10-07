2a4562a0755298783e176d9cfd6d116aa29e1568
lekegitrepo <lekedevrepo@gmail.com>
2020-10-07 02:03:10 -0500

    migrate to database, add validation to the order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_062309) do
+ActiveRecord::Schema.define(version: 2020_10_07_065649) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title", default: ""
@@ -36,4 +44,5 @@ ActiveRecord::Schema.define(version: 2020_10_06_062309) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
