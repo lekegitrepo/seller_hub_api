39a8111d0f24769899713dcbe4fda0e8056793c1
lekegitrepo <lekedevrepo@gmail.com>
2020-10-07 03:39:49 -0500

    add show route for an order

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show, :create, :update, :destroy] do
         resources :products, only: [:create, :update, :destroy]
-        resources :orders, only: [:index]
+        resources :orders, only: [:index, :show]
       end
 
       resources :sessions, only: [:create, :destroy]
