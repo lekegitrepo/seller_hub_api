a68ddfccff91a7abc974f1d3eb14320f7e626121
lekegitrepo <lekedevrepo@gmail.com>
2020-10-08 07:29:23 -0500

    migrate to the database

--- db/migrate/20201008122313_add_quantity_to_products.rb
+++ db/migrate/20201008122313_add_quantity_to_products.rb
@@ -1,5 +1,5 @@
 class AddQuantityToProducts < ActiveRecord::Migration[6.0]
   def change
-    add_column :products, :quantity, :integer
+    add_column :products, :quantity, :integer, default: 0
   end
 end
