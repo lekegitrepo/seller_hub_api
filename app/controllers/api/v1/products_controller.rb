7bc91dc7872d075204cf45bc38b6292e58c5eda7
lekegitrepo <lekedevrepo@gmail.com>
2020-10-09 14:07:26 -0500

    add the paginatio meta info to the json response

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -6,6 +6,10 @@ class Api::V1::ProductsController < ApplicationController
   def index
     # respond_with Product.all
     respond_with Product.search(params).page(params[:page]).per(params[:per_page])
+    render json: products, meta: { pagination:
+                                   { per_page: params[:per_page],
+                                     total_pages: products.total_pages,
+                                     total_objects: products.total_count } }
   end
 
   def show
